26f313643d848b2a6d4677b2365df7b15b000c30
Lê Minh Huy <78388860+huyhuyhuy8s@users.noreply.github.com>
2025-05-20 18:01:13 +0700

    add check email exist

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -423,7 +423,7 @@ export type Database = {
         Update: {
           created_at?: string | null
           id?: string
-          label?: string
+          label: string
           price?: number
           updated_at?: string | null
           value?: string
@@ -571,7 +571,10 @@ export type Database = {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      check_if_email_exists: {
+        Args: { email_to_check: string }
+        Returns: boolean
+      }
     }
     Enums: {
       [_ in never]: never
@@ -593,7 +596,7 @@ export type Tables<
   }
     ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
         Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    : never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
       Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
